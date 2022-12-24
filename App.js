821be45d733452db9d6f4862249fad56b84d414f
aryan2223 <98646730+aryan2223@users.noreply.github.com>
2022-12-24 18:42:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from "./screens/Home";
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+     
+     
+        <Stack.Screen name="Home" component={HomeScreen} />
+    
+    </NavigationContainer>
+  );
+}
+
+export default App;
